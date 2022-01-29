a4fc1a5771630cfbde385e9dc7f085fd130b5190
0639Quercus <thsomsits.brie@gmail.com>
2022-01-29 12:32:21 +0700

    lab21_1.cpp completed

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect A, Rect B){
+	double ovlp_w = 0, ovlp_h = 0;
+	ovlp_w = ((A.x + A.w < B.x + B.w)? A.x + A.w : B.x + B.w) - ((A.x > B.x)? A.x : B.x);
+	if (ovlp_w < 0) ovlp_w = 0;
+	ovlp_h = ((A.y < B.y)? A.y : B.y) - ((A.y - A.h > B.y - B.h)? A.y - A.h : B.y - B.h);
+	if (ovlp_h < 0) ovlp_h = 0;
 
-
-
+	return ovlp_w * ovlp_h;
 }
